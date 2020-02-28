1cff7172dcad7fd7ec8d061056fc0c03ab515dd9
Mihail Angello Motso <anxhelo91moco@gmail.com>
2020-02-28 22:16:50 +0100

    rotating blocks in level 2

---
+++ TetrisHD2/Assets/rotatingPlatforms.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatingPlatforms : MonoBehaviour
+{
+    public GameObject platform1;
+    public GameObject platform2;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(0, 0, speed);
+        platform1.transform.Rotate(0, 0, -speed);
+        platform2.transform.Rotate(0, 0, -speed);
+    }
+}
